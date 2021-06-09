054e5e16cb9238f9417f625e9709f549416b1c7a
Soumay kanti Das <anikds2859@outlook.com>
2021-06-09 12:11:17 +0600

    Add project files.

---
+++ fuction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fuction
+{
+
+    class Program
+
+    {
+        public static bool chkprime(int num)
+        {
+            for(int i= 2; i<=num/i; i++)
+              if (num%i==0)
+                    return false;
+                return true;
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please Enter the number to chek prime ");
+            string var;
+            int num;
+            var = Console.ReadLine();
+            num = Convert.ToInt32(var);
+            Console.WriteLine(chkprime(num));
+        }
+    }
+}
